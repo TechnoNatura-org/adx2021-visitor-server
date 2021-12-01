3d1283fabb015b996382ccf5aace0453ce3f9c10
aldhanekaa <aldhanekadev@gmail.com>
2021-12-02 04:36:29 +0700

    a

--- dist/socket/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function SocketMain(socket) {
-    console.log('A user has connected');
-    socket.on('disconnect', () => {
-        console.log('A user has disconnected');
-    });
-}
-exports.default = SocketMain;
